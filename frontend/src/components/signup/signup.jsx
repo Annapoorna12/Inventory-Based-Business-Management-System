c3b6ebaf7478e76f69fc6ef4596223f19009080d
Annapoorna12 <annapoornaprabhu2002@gmail.com>
2024-02-19 13:09:16 +0530

    updates

--- frontend/src/components/signup/signup.jsx
+++ frontend/src/components/signup/signup.jsx
@@ -1,6 +1,7 @@
 import React,{ useState } from 'react'
 import { Link } from 'react-router-dom'
 import Validation from './signupvalidation';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function Signup() {
 
