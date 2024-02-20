9a793b4f21bf19d914a95e996a78b9aed63fb209
Annapoorna12 <annapoornaprabhu2002@gmail.com>
2024-02-20 22:06:09 +0530

    user changes

--- user/src/App.js
+++ user/src/App.js
@@ -1,29 +1,13 @@
-import logo from './assets/logo white.png';
-import {Link} from 'react-router-dom';
+// import logo from './assets/logo white.png';
+// import {Link} from 'react-router-dom';
 import './App.css';
+import Header from './components/header'
 // import Login from './components/login'
 
 function App() {
   return (
     <> 
-    <div className='homepage'>
-    <nav className='navbar'>
-    <img src={logo} alt="logo" />
-    <div className='menu'>
-        <Link to='/'>Home</Link>
-        <Link to='/Login'> Login</Link>
-        <Link to='/Signup'>Signup</Link>
-        <Link to=''>About </Link>
-        <Link to=''>Contact</Link>
-    </div>
-    </nav>
-    <div className='intro'>
-        <p>
-        Crafting Tomorrow with Today's Finest Bricks
-        </p>
-    </div>
-    
-</div>
+    <Header/>
 </>
   );
 }
---
+++ user/src/components/header.jsx
@@ -0,0 +1,46 @@
+import logo from '../assets/logo white.png';
+import { useDispatch, useSelector } from 'react-redux';
+import {Link} from 'react-router-dom';
+// import './App.css';
+// import Login from './components/login'
+
+function Header() {
+  // const userLogin = useSelector(state => state.userLogin);
+  // const { userInfo } = userLogin;
+  // const dispatch = useDispatch();
+
+  // const logoutHandler = () => {
+  //   dispatch(logout());
+  // };
+
+  return (
+    <> 
+    <div className='homepage'>
+    <nav className='navbar'>
+    <img src={logo} alt="logo" style={{height: '60px',
+  width: '135px', marginLeft: '25px', marginTop: '20px'}} />
+    <div className='menu'>
+        <Link to='/'>Home</Link>
+        <Link to=''>About </Link>
+        <Link to=''>Contact</Link>
+        {/* {userInfo ? (
+            <Link to=''>Cart </Link>
+            <Link to=''><Logout></Logout></Link>
+        ) : ( */}
+        <Link to='/Login'> Login</Link>
+        <Link to='/Signup'>Signup</Link>
+        {/* )} */}
+    </div>
+    </nav>
+    <div className='intro'>
+        <p>
+        Crafting Tomorrow with Today's Finest Bricks
+        </p>
+    </div>
+    
+</div>
+</>
+  );
+}
+
+export default Header;
