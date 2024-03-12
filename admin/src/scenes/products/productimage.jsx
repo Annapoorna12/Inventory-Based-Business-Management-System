579161ce4b26a70560b1f27f6739e008f4559005
Arsha5502 <arshamjoby@gmail.com>
2024-03-12 22:39:49 +0530

    commit

--- admin/src/scenes/products/productimage.jsx
+++ admin/src/scenes/products/productimage.jsx
@@ -1,16 +1,17 @@
-// ProductImage.js
 import React from 'react';
 
-const ProductImage = ({ imageUrl, altText }) => {
-  console.log(imageUrl);
-  return (
-    <img
-      src={imageUrl}  
-      alt={altText}
-      style={{ width: 50, height: 50 }}  
-      onLoad={() => console.log(`Image loaded: ${imageUrl}`)}
-      onError={() => console.error(`Error loading image: ${imageUrl}`)}
-    />
+
+  const ProductImage = ({ imageUrl, altText }) => {
+    console.log(imageUrl);
+    return (
+      <img
+        src={imageUrl}  
+        alt={altText}
+        style={{ width: 50, height: 50 }}  
+        onLoad={() => console.log(`Image loaded: ${imageUrl}`)}
+        onError={() => console.error(`Error loading image: ${imageUrl}`)}
+      />
+    
   );
 };
 
